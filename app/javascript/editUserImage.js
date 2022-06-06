f48010d96296482af2328bb8185594ac792496f2
ShoheiTakino <shohei0522penguin@gmail.com>
2022-06-06 18:50:14 +0900

    ユーザー情報編集画面のプレビュー実装

--- app/javascript/editUserImage.js
+++ app/javascript/editUserImage.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const postForm = document.getElementById('new_post');
-  const previewList = document.getElementById('previews');
-  if (!postForm) return null;
+  const editForm = document.getElementById('icon_edit');
+  const editNew = document.getElementById('icon__edit__area');
+  if (editForm) return null;
+  
+  const editField = document.querySelector('input[type="file"][name="user[profile_image]"]');
+  editField.addEventListener('change', function(e){
 
-  const fileArea = document.querySelector('input[type="file"][name="post[image]"]');
-  fileArea.addEventListener('change', function(e){
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview){
+    const alreadyPreview = document.querySelector('.edit__user__image');
+    if (alreadyPreview) {
       alreadyPreview.remove();
-      };
-      
+    };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+
     const previewWrapper = document.createElement('div');
-    previewWrapper.setAttribute('class', 'preview');
+    previewWrapper.setAttribute('class', 'edit__user__image');
     const previewImage = document.createElement('img');
-    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('class', 'preview__image');
     previewImage.setAttribute('src', blob);
 
     previewWrapper.appendChild(previewImage);
-    previewList.appendChild(previewWrapper);
-  })
-})
+    editNew.appendChild(previewWrapper);
+  });
+});
