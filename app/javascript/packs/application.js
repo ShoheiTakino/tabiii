20101269b8688444d71b27c27cf026a70f7e25a1
ShoheiTakino <shohei0522penguin@gmail.com>
2022-06-08 17:05:25 +0900

    posts#index画像埋め込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("../editUserImage")
 require('jquery')
 
 require('sticky.js')
-require('slideShow.js')
+//require('slideShow.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
