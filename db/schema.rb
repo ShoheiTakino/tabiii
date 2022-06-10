0a66c8f97172ac339a72e60e36ca2f551d5af83e
ShoheiTakino <shohei0522penguin@gmail.com>
2022-06-10 10:36:38 +0900

    googleユーザー認証機能実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_112127) do
+ActiveRecord::Schema.define(version: 2022_06_09_131609) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2022_06_04_112127) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,4 +102,5 @@ ActiveRecord::Schema.define(version: 2022_06_04_112127) do
   add_foreign_key "follows", "users"
   add_foreign_key "follows", "users", column: "follow_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "sns_credentials", "users"
 end
